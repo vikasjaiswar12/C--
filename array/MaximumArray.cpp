f6f14e71ace715642e8705cab31143c7a1923632
Vikas Jaiswar <vikasjaiswar120508@gmail.com>
2026-03-17 20:07:55 +0530

    Maxmimum array solved

---
+++ array/MaximumArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+ int arr[6];
+  for(int i=0;i<6;i++){
+    cin>>arr[i];
+  }
+  int start=0;
+  int end=5;
+  while(start<=end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+  }
+ for(int i=0; i<6; i++){
+    cout<<arr[i]<<" ";
+ }
+}
